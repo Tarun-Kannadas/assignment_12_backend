c6b1c3930a0c7853264b9260965b50ac19eec68b
Tarun Kannadas <68431130+Tarun-Kannadas@users.noreply.github.com>
2025-11-04 19:13:52 +0530

    initial commit

--- server.js
+++ server.js
@@ -10,36 +10,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/my_resumeDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.log("❌ DB Error:", err));
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("DB Error:", err));
 
-//Route to test
 app.get("/", (req, res) => {
   res.send("Resume API is running...");
 });
 
-//Get Resume Data
 app.get("/api/resume", async (req, res) => {
-  try {
+  try 
+  {
     const resume = await Resume.findOne();
     res.json(resume);
-  } catch (err) {
+  } 
+  catch (err) 
+  {
     res.status(500).json({ message: err.message });
   }
 });
 
-//Add Resume Data (insert once using Hoppscotch/Postman)
 app.post("/api/resume", async (req, res) => {
-  try {
+  try 
+  {
     const resume = new Resume(req.body);
     await resume.save();
     res.status(201).json(resume);
-  } catch (err) {
+  } 
+  catch (err) 
+  {
     res.status(400).json({ message: err.message });
   }
 });
